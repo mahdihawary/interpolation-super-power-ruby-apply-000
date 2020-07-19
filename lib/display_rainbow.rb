ec20d08f98e8d36710bd9262d26579442d90b2d7
Mahdi Hawary <mahdihawary@gmail.com>
2020-07-19 06:09:41 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -2,4 +2,4 @@
 def display_rainbow (rain_array)
   puts "R: #{rain_array[0]}, O: #{rain_array[1]}, Y: #{rain_array[2]}, G: #{rain_array[3]}, B: #{rain_array[4]}, I: #{rain_array[5]}, V: #{rain_array[6]}"
 end
-display_rainbow(['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
+display_rainbow(['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'])
